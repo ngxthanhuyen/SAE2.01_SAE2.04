dd2055d852f26d40e234070819a59994865a32bd
ngxthanhuyen <nthanhuyen1411@gmail.com>
2024-05-11 22:20:30 +0200

    commit

---
+++ model.py
@@ -0,0 +1,85 @@
+import sqlite3
+class DataBase:
+    def __init__(self):
+        self.conn = sqlite3.connect('SAE204.db')
+        self.cursor = self.conn.cursor()
+        self.create_tables()
+    def create_tables(self):
+        try:
+            self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS DEPARTEMENTS (
+              id_departement INTEGER PRIMARY KEY AUTOINCREMENT,
+              code_departement TEXT UNIQUE NOT NULL,
+              nom_departement TEXT
+            );''')
+            self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS COMMUNES (
+              id_commune INTEGER PRIMARY KEY AUTOINCREMENT,
+              code_commune_insee TEXT UNIQUE NOT NULL,
+              nom_commune TEXT
+            );''')
+            self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS MASSE_EAU (
+              id_masse_eau INTEGER PRIMARY KEY AUTOINCREMENT,
+              code_masse_eau_edl TEXT UNIQUE NOT NULL,
+              nom_masse_eau_edl TEXT,
+              urn_masse_eau_edl TEXT
+            );''')
+            self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS STATIONS (
+              id_station INTEGER PRIMARY KEY AUTOINCREMENT,
+              id_commune INTEGER NOT NULL,
+              id_departement INTEGER NOT NULL,
+              id_masse_eau INTEGER NOT NULL,
+              code_bss TEXT,
+              bss_id TEXT,
+              urn_bss TEXT,
+              profondeur_investigation REAL,
+              x REAL,
+              y REAL,
+              altitude_station REAL,
+              nb_mesures_piezo INTEGER,
+              FOREIGN KEY (id_commune) REFERENCES COMMUNES(id_commune),
+              FOREIGN KEY (id_departement) REFERENCES DEPARTEMENTS(id_departement),
+              FOREIGN KEY (id_masse_eau) REFERENCES MASSE_EAU(id_masse_eau)
+            );''')
+            self.conn.commit()
+        except Exception as e:
+            print(f"Il y a une erreur{e}")
+
+    def insert_dept(self, code_departement, nom_departement):
+        self.cursor.execute('INSERT OR IGNORE INTO DEPARTEMENTS (code_departement, nom_departement) VALUES (?,?)',
+                            (code_departement, nom_departement))
+        self.conn.commit()
+    def insert_commune(self, code_commune_insee, nom_commune):
+        self.cursor.execute('INSERT OR IGNORE INTO COMMUNES (code_commune_insee, nom_commune) VALUES (?,?)',
+                            (code_commune_insee, nom_commune))
+        self.conn.commit()
+    def insert_masse_eau(self, code_masse_eau, nom_masse_eau, urn_masse_eau):
+        self.cursor.execute('INSERT OR IGNORE INTO MASSE_EAU(code_masse_eau_edl, nom_masse_eau_edl, urn_masse_eau_edl) VALUES (?,?,?)',
+                            (code_masse_eau, nom_masse_eau, urn_masse_eau))
+        self.conn.commit()
+    def insert_station(self, id_commune, id_departement, id_masse_eau, code_bss, bss_id, urn_bss, profondeur_investigation, x, y, altitude_station, nb_mesures_piezo):
+        self.cursor.execute('INSERT OR IGNORE INTO STATIONS (id_commune, id_departement, id_masse_eau, code_bss, bss_id, urn_bss, profondeur_investigation, x, y, altitude_station, nb_mesures_piezo) VALUES (?,?,?,?,?,?,?,?,?,?,?)',
+                            (id_commune, id_departement, id_masse_eau, code_bss, bss_id, urn_bss, profondeur_investigation, x, y, altitude_station, nb_mesures_piezo))
+        self.conn.commit()
+    def get_id_commune(self, code_commune_insee):
+        query = "SELECT id_commune FROM COMMUNES WHERE code_commune_insee = ?"
+        self.cursor.execute(query, (code_commune_insee,))
+        result = self.cursor.fetchone()
+        return result[0] if result else None
+
+    def get_id_departement(self, code_departement):
+        query = "SELECT id_departement FROM DEPARTEMENTS WHERE code_departement = ?"
+        self.cursor.execute(query, (code_departement,))
+        result = self.cursor.fetchone()
+        return result[0] if result else None
+
+    def get_id_masse_eau(self, code_masse_eau):
+        query = "SELECT id_masse_eau FROM MASSE_EAU WHERE code_masse_eau_edl = ?"
+        self.cursor.execute(query, (code_masse_eau,))
+        result = self.cursor.fetchone()
+        return result[0] if code_masse_eau else 0
+    def close(self):
+        self.conn.close()
+    
---
+++ script.py
@@ -0,0 +1,75 @@
+#####################################################################
+# IMPORTATION DES MODULES
+#####################################################################
+from model import DataBase
+import requests
+
+#On initialise l'instance de la base de données
+db = DataBase()
+#####################################################################
+# FONCTIONS AUXILIAIRES POUR RÉCUPÉRATION DES ID
+#####################################################################
+def get_id_commune(code_commune_insee):
+   query = "SELECT id_commune FROM COMMUNES WHERE code_commune_insee = ?"
+   db.cursor.execute(query,(code_commune_insee,))
+   result = db.cursor.fetchone()
+   return result[0] if result else None
+
+def get_id_departement(code_departement):
+   query = "SELECT id_departement FROM DEPARTEMENTS WHERE code_departement = ?"
+   db.cursor.execute(query,(code_departement,))
+   result = db.cursor.fetchone()
+   return result[0] if result else None
+
+def get_id_masse_eau(code_masse_eau):
+   query = "SELECT id_masse_eau FROM MASSE_EAU WHERE code_masse_eau_edl = ?"
+   db.cursor.execute(query, (code_masse_eau,))
+   result = db.cursor.fetchone()
+   return result[0] if code_masse_eau else 0
+
+#####################################################################
+# CHARGEMENT ET TRAITEMENT DES DONNÉES
+#####################################################################
+def load_data():
+    URL = "https://hubeau.eaufrance.fr/api/v1/niveaux_nappes/stations?format=json"
+    response = requests.get(URL)
+    #On vérifie si la requête a réussi (avec code de statut 200), ou partiellement réussi (avec code de statut = 206)
+    if response.status_code == 200 or response.status_code == 206:
+        data = response.json()
+        if 'data' in data:
+            #On insère les données dans la base de données
+            for entry in data['data']:
+              #Insertion des informations des départements dans la base de données
+              db.insert_dept(entry['code_departement'], entry['nom_departement'])
+              #Insertion des informations des communes dans la base de données
+              db.insert_commune(entry['code_commune_insee'], entry['nom_commune'])
+
+              #On s'assure que les champs sont des listes avant d'essayer d'accéder à leurs éléments
+              code_masse_eau = entry.get('codes_masse_eau_edl')
+              nom_masse_eau = entry.get('noms_masse_eau_edl')
+              urn_masse_eau = entry.get('urns_masse_eau_edl')
+
+              #On récupère le premier élément de chaque liste s'il existe et 'None' si la liste est vide
+              code_masse_eau = code_masse_eau[0] if code_masse_eau else None
+              nom_masse_eau = nom_masse_eau[0] if nom_masse_eau else None
+              urn_masse_eau = urn_masse_eau[0] if urn_masse_eau else None
+              #On vérifie si toutes les informations nécessaires sont présentes avant d'insérer
+              if code_masse_eau and nom_masse_eau and urn_masse_eau:
+                db.insert_masse_eau(code_masse_eau, nom_masse_eau, urn_masse_eau) 
+                
+              #Insertion des informations des stations après récupération des IDs nécessaires des autres tables
+              
+              id_commune = get_id_commune(entry['code_commune_insee'])
+              id_departement = get_id_departement(entry['code_departement'])
+              id_masse_eau = get_id_masse_eau(code_masse_eau)
+              if id_commune and id_departement and id_masse_eau:
+                db.insert_station(id_commune, id_departement, id_masse_eau, entry['code_bss'], entry['bss_id'], entry['urn_bss'], entry['profondeur_investigation'], entry['x'], entry['y'], entry['altitude_station'], entry['nb_mesures_piezo'])           
+            return "Données chargées avec succès"
+        else: 
+            return "Il y a aucune donnée disponible"
+    else:
+        raise Exception(f"Erreur HTTP {response.status_code}")
+  
+
+if __name__ == "__main__":
+    print(load_data())
